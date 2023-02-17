3ff16e5db3d88e9e487f7665b7a37b017e8a66e8
Svetlana <lavsvetlav@gmail.com>
2023-02-18 00:29:18 +0300

    some fixes

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package org.example.repository;
 
+import jakarta.validation.constraints.NotNull;
 import org.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     User findByChatId(Long chatId);
